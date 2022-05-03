8b75efc888f90b10d82b1ae2ba08f657edb22697
dnckdgml33 <50548939+dnckdgml33@users.noreply.github.com>
2022-05-03 21:01:34 +0900

    20220503_3
    굿

---
+++ 20220503_3/cookKing/app/src/main/java/com/example/cookking/CustomDialog.java
@@ -0,0 +1,68 @@
+package com.example.cookking;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
+public class CustomDialog extends Dialog {
+    private TextView txt_contents;
+    private Button shutdownClick;
+   //ArrayList<SampleData> movieDataList;
+
+    static int either = 0;
+    public CustomDialog(@NonNull Context context, String contents) {
+        super(context);
+        setContentView(R.layout.activity_custom_dialog);
+        //MyAdapter myAdapter = new MyAdapter(this,movieDataList);
+
+        EditText customRecipe = (EditText) this.findViewById(R.id.customRecipe);
+        EditText customMaterial = (EditText) this.findViewById(R.id.customMaterial);
+
+        shutdownClick = findViewById(R.id.btnDone);
+        shutdownClick.setOnClickListener(new View.OnClickListener() {
+
+
+            protected void onCreate(Bundle savedInstanceState) {
+
+
+
+            }
+            @Override
+            public void onClick(View v) {
+
+                if(either == 1) {
+                    //String recipeName = (customRecipe.getText().toString());
+                    MainActivity.recipeName = (customRecipe.getText().toString());
+                    //String recipeMaterial = customMaterial.getText().toString();
+                    MainActivity.recipeMaterial = customMaterial.getText().toString();
+
+                    MainActivity.flag1 = 1;
+                }
+                else{
+                    MainActivity.Material = (customRecipe.getText().toString());
+
+                    MainActivity.materialExpireDate = customMaterial.getText().toString();
+                    MainActivity.flag2 = 1;
+
+                }
+
+                /*int count = myAdapter.getCount();
+                // 아이템 추가.
+                movieDataList.add(new SampleData(R.drawable.plus, recipeName,recipeMaterial));
+                // listview 갱신
+                myAdapter.notifyDataSetChanged();*/
+
+
+                dismiss();
+            }
+        });
+    }
+}
