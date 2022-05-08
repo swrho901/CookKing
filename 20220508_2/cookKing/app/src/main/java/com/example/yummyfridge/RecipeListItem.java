60f1d07af256f08d59c059a80166916cd7285f6b
dnckdgml33 <50548939+dnckdgml33@users.noreply.github.com>
2022-05-08 15:39:19 +0900

    20220508_2
    asdf

---
+++ 20220508_2/cookKing/app/src/main/java/com/example/yummyfridge/RecipeListItem.java
@@ -0,0 +1,28 @@
+package com.example.yummyfridge;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+public class RecipeListItem {
+    private String text1;
+    private String text2;
+
+    public String getText1() {
+        return text1;
+    }
+
+    public void setText1(String text1) {
+        this.text1 = text1;
+    }
+
+    public String getText2() {
+        return text2;
+    }
+
+    public void setText2(String text2) {
+        this.text2 = text2;
+    }
+
+}
+
